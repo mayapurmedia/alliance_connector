e9ed6a0233968eed3236eba7dcc1224404f14938
Petr Vacha / Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2016-11-12 22:49:08 +0530

    When logged in, but no internet, try to logout.

--- alliance_connector.sh
+++ alliance_connector.sh
@@ -15,10 +15,12 @@ then
         # Using wget
         LOGIN_CHECK="wget -qO- \"http://$ALLIANCE_IP/0/wl/cklogin\""
         LOGIN="wget --user-agent=Mozilla/5.0 --post-data \"login=1&user=$ALLIANCE_USER&pass=$ALLIANCE_PASS\" -q --no-check-certificate \"http://$ALLIANCE_IP/0/wl/\""
+        LOGOUT="wget --user-agent=Mozilla/5.0 --post-data \"logout=Click+here+to+logout\" -q --no-check-certificate \"http://$ALLIANCE_IP/0/up/\""
     else
         # Using curl
         LOGIN_CHECK="curl -s \"http://$ALLIANCE_IP/0/wl/cklogin\""
         LOGIN="curl --data \"login=1&user=$ALLIANCE_USER&pass=$ALLIANCE_PASS\" \"http://$ALLIANCE_IP/0/wl/\""
+        LOGOUT="curl --data \"logout=Click+here+to+logout\" \"http://$ALLIANCE_IP/0/up/\""
     fi
 else
     echo "Cannot find wget or curl. Please install one of them."
@@ -30,13 +32,20 @@ echo "Checking login status..."
 if [[ $(eval $LOGIN_CHECK) == "yes" ]]
 then
     echo "You are already logged in."
-else
-    echo "Trying to login..."
-    eval $LOGIN
-    if [[ $(eval $LOGIN_CHECK) == "yes" ]]
-    then
-        echo "Login successful."
+    if [[ "$(ping -c 1 -t 5 8.8.8.8 | grep '100% packet loss' )" != "" ]]; then
+        echo "Internet isn't present, logging you out"
+        eval $LOGOUT
     else
-        echo "Couldn't login. Please check your user details in config.cfg file or your network connection."
+        echo "Internet is present"
+        exit 1
     fi
+fi
+
+echo "Trying to login..."
+eval $LOGIN
+if [[ $(eval $LOGIN_CHECK) == "yes" ]]
+then
+    echo "Login successful."
+else
+    echo "Couldn't login. Please check your user details in config.cfg file or your network connection."
 fi
